6d15f396ae68cb5d3714a15aa3a01aaa2c137d5d
dainn <nguyendai060703@gmail.com>
2025-09-22 09:45:16 +0700

    refactor: domain and configs

--- src/constants/apiConfig.ts
+++ src/constants/apiConfig.ts
@@ -1,3 +1,4 @@
 export const ApiConfig = {
-  difyServerUrl: "https://386eeea1207e.ngrok-free.app/v1/chat-messages",
+  // difyServerUrl: "https://386eeea1207e.ngrok-free.app/v1/chat-messages",
+  difyServerUrl: "http://dify.alphazteam.xyz/v1/chat-messages",
 };
