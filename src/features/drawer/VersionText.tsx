eb73f6667d2ffedd4cee6e610a64296385bd381c
dainn <nguyendai060703@gmail.com>
2025-11-07 22:33:05 +0700

    feat: version text

--- src/features/drawer/VersionText.tsx
+++ src/features/drawer/VersionText.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import { AppConfig } from "../../constants/appConfig";
 import { CustomText } from "../../components/text/customText";
 import { useAppTheme } from "../../theme";
-import { useTranslation } from "react-i18next";
 
 export const VersionText = () => {
   const { colors } = useAppTheme();
-  const { t } = useTranslation();
 
   return (
     <CustomText
@@ -18,6 +16,6 @@ export const VersionText = () => {
         fontStyle: "italic",
         color: colors.text,
       }}
-    >{`${t("drawer_version")}: ${AppConfig.version} ${AppConfig.devMode ? `(${AppConfig.buildVersion})` : ""}`}</CustomText>
+    >{`v${AppConfig.version} ${AppConfig.devMode ? `(${AppConfig.buildVersion})` : ""}`}</CustomText>
   );
 };
