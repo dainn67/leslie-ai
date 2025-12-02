588fe9461486b455e15ab144a957d7eda3a1755f
dainn <nguyendai060703@gmail.com>
2025-12-03 00:35:34 +0700

    ad id

--- src/constants/adsConfig.ts
+++ src/constants/adsConfig.ts
@@ -1,4 +1,5 @@
 export const AdsConfig = {
   bannerId: "ca-app-pub-6011704237608953/2310302283",
   rewardedId: "ca-app-pub-6011704237608953/2109818271",
+  interstitialId: "ca-app-pub-6011704237608953/7500693405",
 };
