e6aad5e25b2710f40aaf71fe9a2f3081c7e6e61c
dainn <nguyendai060703@gmail.com>
2025-12-03 11:31:08 +0700

    fix: banner ads

--- src/features/ads/BannerAds.tsx
+++ src/features/ads/BannerAds.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import { BannerAd, BannerAdSize, TestIds } from "react-native-google-mobile-ads";
 import { AdsConfig, AppConfig } from "../../constants";
@@ -6,9 +6,24 @@ import { AdsConfig, AppConfig } from "../../constants";
 const bannerId = AppConfig.devMode ? TestIds.BANNER : AdsConfig.bannerId;
 
 export const BannerAds = () => {
+  const [adLoaded, setAdLoaded] = useState(false);
+
+  const handleAdLoaded = () => {
+    setAdLoaded(true);
+  };
+
+  const handleAdFailedToLoad = () => {
+    setAdLoaded(false);
+  };
+
   return (
-    <View style={{ alignItems: "center" }}>
-      <BannerAd unitId={bannerId} size={BannerAdSize.BANNER} />
+    <View style={{ alignItems: "center", height: adLoaded ? undefined : 0 }}>
+      <BannerAd
+        unitId={bannerId}
+        size={BannerAdSize.BANNER}
+        onAdLoaded={handleAdLoaded}
+        onAdFailedToLoad={handleAdFailedToLoad}
+      />
     </View>
   );
 };
