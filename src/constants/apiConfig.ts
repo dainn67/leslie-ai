2795391af8797f658288da36b9a99c044fd71168
dainn <nguyendai060703@gmail.com>
2026-02-13 08:55:19 +0700

    fix: api

--- src/constants/apiConfig.ts
+++ src/constants/apiConfig.ts
@@ -1,3 +1,3 @@
 export const ApiConfig = {
-  dafaultDifyServerUrl: "https://3dcad279c0b4.ngrok-free.app",
+  dafaultDifyServerUrl: "https://4138-2405-4802-1d39-c3e0-a8b6-cb7d-92a5-977f.ngrok-free.app",
 };
