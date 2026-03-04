1deb677de9f2796fd0bc263ea0f7e8ad2b5d9e27
dainn <nguyendai060703@gmail.com>
2026-03-04 09:39:17 +0700

    versioning

--- app.config.js
+++ app.config.js
@@ -4,11 +4,11 @@ export default {
     name: "AIkaze",
     slug: "leslieai",
     icon: "./assets/images/app-logo.png",
-    version: "1.5.7",
+    version: "1.5.8",
     android: {
       package: "com.leslie.app",
       googleServicesFile: "./google-services.json",
-      versionCode: 55,
+      versionCode: 56,
       adaptiveIcon: {
         foregroundImage: "./assets/images/app-logo.png",
         backgroundColor: "#ffffff",
